0b9b16ec7e61f7218545328df21ef8cca9f53ea6
Niharika <n7195338@gmail.com>
2025-10-27 15:38:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
   API_BASE: 'https://api.amitdas.site/Pinterest/api/',
-  WHATSAPP_INSTANCE_ID: '68FB42D2759E7',
-  WHATSAPP_ACCESS_TOKEN: '68e6e742b9f1c'
+  WHATSAPP_INSTANCE_ID: '68FCA18ACD6BA',
+  WHATSAPP_ACCESS_TOKEN: '68fca002c1bf4'
 };
